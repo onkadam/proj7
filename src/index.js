01c2600cfd13a3817eb07f02660fb37892f33e8e
onkadam <onkark3737@gmail.com>
2023-02-13 11:44:01 +0530

    com

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 
+//function component
+function A(props){
+  let name="Onkar"
+  return <span>A - {name} {props.surname} {props.state}<B surname="Gaikwad"/></span>
+}
+
+//class component
+//class child extends parent 
+class B extends React.Component{
+ name="Yogesh";
+  render(){
+    return <h4>B - {this.name} {this.props.surname}<C> Chaugule </C></h4>
+  }
+}
+
+//ES6 Fat arrow function
+let C = (props)=>{
+  let name="Santosh"
+  return <h2>C - {name} {props.children}</h2>
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+let state = "Maharashtra";
+root.render(<A surname="Kadam" state={"Maharashtra"}/>);
 
 
